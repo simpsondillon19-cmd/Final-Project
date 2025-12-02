44c4c0c528a4db37349cda435249e274df3255b8
Your Name <simpsond@ewcmail.wy.edu>
2025-12-02 14:15:55 -0700

    first commit

---
+++ finalproject.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  1 15:28:33 2025
+
+@author: simps
+"""
+
+# choice survival game- you can go either to the woods, river, castle or desert 
+
+playing = True
+
+while playing:
+    choice1= input("Choose where you want to start the game woods, river, castle or desert ")
+    if input== woods :
+   #     import file for each choice of destination
+    
